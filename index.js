3c1aa4c015902ffe97a0937a9238a1a9b55564eb
Niloy Saha <sahaniloy389@gmail.com>
2025-08-13 19:52:16 +0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+$('#linkedin').on('click', function() {
+    window.location.href = 'https://www.linkedin.com/in/niloysaha35';
+  });
+
+  $('#github').on('click', function() {
+    window.location.href = 'https://github.com/NiloySaha84';
+  });
