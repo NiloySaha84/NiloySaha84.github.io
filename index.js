616725a4b0b287122829fb6f144f46a58063d0f9
Niloy Saha <sahaniloy389@gmail.com>
2025-09-13 22:39:31 -0400

    Whole website newly built

--- index.js
+++
@@ -1,7 +0,0 @@
-$('#linkedin').on('click', function() {
-    window.location.href = 'https://www.linkedin.com/in/niloysaha35';
-  });
-
-  $('#github').on('click', function() {
-    window.location.href = 'https://github.com/NiloySaha84';
-  });
